9d91958f2d423fb5e1f97f46968719954a5985b7
Ian Jackson <ijacksondesign@gmail.com>
2021-09-14 14:26:12 -0600

    added const uri to server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
+const uri = process.env.MONGODB_URI;
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
